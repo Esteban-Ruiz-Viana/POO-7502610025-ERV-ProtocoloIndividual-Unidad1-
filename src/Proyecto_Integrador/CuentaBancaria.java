d261e2eb29b57357b1d03448b5fc4e1b29dd1e58
Esteban Ruiz Viana <eruiz@unicartagena.edu.co>
2026-09-17 13:35:21 -0500

    feat(cuenta): agrega clase CuentaBancaria

--- src/Proyecto_Integrador/CuentaBancaria.java
+++ src/Proyecto_Integrador/CuentaBancaria.java
@@ -1,4 +1,31 @@
 package Proyecto_Integrador;
 
 public class CuentaBancaria {
+
+    String numeroCuenta;
+    double saldo;
+    String tipoCuenta;
+
+    public CuentaBancaria() {
+
+        numeroCuenta = "0000";
+        saldo = 0;
+        tipoCuenta = "Ahorros";
+
+    }
+
+    public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
+
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipoCuenta = tipoCuenta;
+
+    }
+
+    @Override
+    public String toString() {
+
+        return "Cuenta " + numeroCuenta + " (" + tipoCuenta + ") - Saldo: $" + saldo;
+
+    }
 }
